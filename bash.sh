38ebe44ca56b56ddf450ec10bba51e38c68ec598
Aung Myo Kyaw <athuyrain@gmail.com>
2026-02-16 12:56:42 +0630

    Delete bash.sh

--- bash.sh
+++
@@ -1,7 +0,0 @@
-# Root directory တည်ဆောက်ပါ
-mkdir -p burmeweb.github.io/{pages,assets/{css,js,img/icons,fonts},data,config}
-
-# အခြေခံဖိုင်တွေ ဖန်တီးပါ
-touch burmeweb.github.io/README.md
-touch burmeweb.github.io/LICENSE
-touch burmeweb.github.io/CONTRIBUTING.md
