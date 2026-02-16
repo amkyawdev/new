91f35daf3b96ddf6a11d217b9bf6cd4a7630e90d
Aung Myo Kyaw <athuyrain@gmail.com>
2026-02-16 13:00:45 +0630

    Update GEMINI_API_KEY in env.js

--- config/env.js
+++ config/env.js
@@ -1,6 +1,6 @@
 // config/env.js
 export const ENV = {
-    GEMINI_API_KEY: "YOUR_GEMINI_API_KEY",
+    GEMINI_API_KEY: "AIzaSyBL1OZxLYxmkN1jpFXZEoZzN-ocFU3yK0Q",
     APP_NAME: "Burme AI",
     APP_VERSION: "1.0.0"
 };
